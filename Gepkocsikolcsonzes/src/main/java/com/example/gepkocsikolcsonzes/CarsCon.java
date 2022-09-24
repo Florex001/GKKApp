d9ea71ea6cadae4182e6695e82b179c10ab7581e
Florex001 <98150600+Florex001@users.noreply.github.com>
2022-09-24 23:00:21 +0200

    Gkk App v0.9.1

--- Gepkocsikolcsonzes/src/main/java/com/example/gepkocsikolcsonzes/CarsCon.java
+++ Gepkocsikolcsonzes/src/main/java/com/example/gepkocsikolcsonzes/CarsCon.java
@@ -13,7 +13,6 @@ import javafx.stage.FileChooser;
 import java.io.*;
 import java.net.URL;
 import java.sql.*;
-import java.util.Base64;
 import java.util.ResourceBundle;
 
 public class CarsCon implements Initializable {
@@ -52,15 +51,6 @@ public class CarsCon implements Initializable {
     @FXML
     private TextField Price_update_TF;
 
-    @FXML
-    private Button open;
-
-    @FXML
-    private ImageView carsimage;
-
-    @FXML
-    private TextField cars_name_tf;
-    File file = null;
 
     int index = -1;
 
@@ -85,8 +75,6 @@ public class CarsCon implements Initializable {
         info_lab.setText("Napi ára: " + dailyprice_TC.getCellData(index).toString() + "Ft.");
         Info_update_TA.setText(info_TC.getCellData(index).toString());
         Price_update_TF.setText(dailyprice_TC.getCellData(index).toString());
-        cars_name_tf.setText(CarName_TC.getCellData(index));
-        carsimage.setImage((Image) image_tc.getCellData(index));
     }//kiválasztott elem bekerül a labelek mellé
 
     @FXML
@@ -101,52 +89,13 @@ public class CarsCon implements Initializable {
         help_alert.show();
     }
 
-    @FXML
-    private void delete_btn(){
-        String car_id_del = id_TC.getCellData(index);
-
-        PreparedStatement pst = null;
-
-        try {
-            Connection con = DBConnector.getConnection();
-
-            String sql = "DELETE FROM `vehicles` WHERE `vehicles`.`id` = "+ car_id_del +";";
-
-            pst = con.prepareStatement(sql);
-            pst.execute();
-            cars_TW.getItems().clear();
-            carstable();
-
-            Alert apply_del_alert = new Alert(Alert.AlertType.CONFIRMATION);
-            apply_del_alert.setTitle("Sikeres törlés.");
-            apply_del_alert.setHeaderText("Sikeresen törölte az adatokat!");
-            apply_del_alert.initOwner(WelcomeCon.cars_window);
-            apply_del_alert.show();
-
-        } catch (SQLException e) {
-            Alert error_update_alert = new Alert(Alert.AlertType.CONFIRMATION);
-            error_update_alert.setTitle("Hiba");
-            error_update_alert.setHeaderText("Az adatbázis nem tud csatlakozni!");
-            error_update_alert.setContentText("Próbálja újra!");
-            error_update_alert.initOwner(WelcomeCon.cars_window);
-            error_update_alert.show();
-        }
-        cars_name_tf.setText("");
-        Info_update_TA.setText("");
-        Price_update_TF.setText("");
-    }
 
     @FXML
     private void update_btn(){
-        index = cars_TW.getSelectionModel().getSelectedIndex();
-        if (index <= -1){
-            return;
-        }
 
         PreparedStatement pst = null;
 
         String id = id_TC.getCellData(index).toString();
-        String car_name = cars_name_tf.getText();
         String info = Info_update_TA.getText();
         String price = Price_update_TF.getText();
 
@@ -154,8 +103,7 @@ public class CarsCon implements Initializable {
         try {
             Connection con = DBConnector.getConnection();
 
-            String update = "UPDATE `vehicles` SET `car` = '"+car_name+"', `info` = '"+info+"', `daily_price` = '"+price+"' WHERE `vehicles`.`id` = "+id+";";
-
+            String update = "UPDATE `vehicles` SET `info` = '"+info+"', `daily_price` = '"+price+"' WHERE `vehicles`.`id` = "+id+";";
 
             pst = con.prepareStatement(update);
             pst.execute();
@@ -175,6 +123,7 @@ public class CarsCon implements Initializable {
             error_update_alert.setContentText("Próbálja újra, vagy tegyen bejelentést! +36709312755");
             error_update_alert.initOwner(WelcomeCon.reservation_window);
             error_update_alert.show();
+            e.printStackTrace();
         }
 
         Info_update_TA.setText("");
@@ -185,9 +134,6 @@ public class CarsCon implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         carstable();
-        open.setOnAction(e->{
-            chooseFile();
-        });
     }
 
     private void carstable(){
@@ -219,26 +165,7 @@ public class CarsCon implements Initializable {
         }
     }//lekérdezek mindent a vehicles táblából és azokat eltárolom a carslist ben és utána kiiratom a táblázatban
 
-    private void chooseFile() {
-        FileChooser filechooser = new FileChooser();
-        file = filechooser.showOpenDialog(open.getScene().getWindow());
 
-        try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-
-            Image image = new Image(fileInputStream);
-            carsimage.setImage(image);
-
-
-        } catch (FileNotFoundException e) {
-            Alert no_file = new Alert(Alert.AlertType.ERROR);
-            no_file.setTitle("Error");
-            no_file.setHeaderText("Nem választott ki fájlt.");
-            no_file.setContentText(".JPEG, .JPG fájl tölthető fel.");
-            no_file.initOwner(WelcomeCon.cars_window);
-            no_file.show();
-        }
-    }//fájl kiválasztáshoz
 
 }
 
